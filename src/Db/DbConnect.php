db18026ff0c978e3b7af5021b45e09df0c776a3a
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2024-01-30 16:43:45 +0100

    creation dbConnect

---
+++ src/Db/DbConnect.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace Gdesplantes\PhpLibrary\Db;
+
+use Exception;
+use PDO; 
+
+/**
+ * Connexion à la base de données (SINGLETON)
+ */
+class DbConnect
+{
+
+    private static array $config;
+    /**
+     * Constructeur privé (la classe n'est pas instanciable)
+     */
+    private function __construct() {}
+
+    /** @var PDO $instance l'instance PDO unique */
+    private static ?PDO $instance = null;
+
+    /**
+     * Récupère l'instance unique de PDO
+     * La crée si elle n'existe pas encore
+     * @return PDO l'instance de PDO
+     */
+    public static function getInstance(): PDO
+    {
+        if(self::$instance === null) {
+            self::$instance = new PDO('mysql:host='.self::$config['db_host'].';port='.self::$config['db_port'].';dbname='.self::$config['db_name'].';charset=utf8', 
+            self::$config['db_user'], self::$config['db_password'], [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+        }
+        return self::$instance;
+    }
+
+    public static function setConfiguration(string $chemin): void
+    {
+        if(is_file($chemin)) {
+            self::$config = require $chemin;
+            var_export(self::$config);
+        } else {
+            throw new Exception('Configuration base de données invalide');
+        }
+    }
+}
